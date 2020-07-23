87d72b9a42a5da4f49b4dfb8f21d9402bddc7dfb
Mike Sadler <msadler@raybeam.com>
2020-07-23 13:35:46 -0400

    Feature/airflow config test (#1)

--- tests/test_compatibility.py
+++ tests/test_compatibility.py
@@ -1,7 +1,19 @@
 import pytest
+from airflow.configuration import conf
+import logging
 
 
 @pytest.mark.compatibility
-def test_sample():
-    print("rb template compatibility test was called.")
-    return True
+@pytest.mark.parametrize(
+    ("curr_section", "curr_key", "expected_value"),
+    [
+        ("core", "enable_xcom_pickling", "False"),
+    ],
+)
+def test_check_configuration_value(curr_section, curr_key, expected_value):
+    """
+    Checks that airflow.cfg's configurations are set properly for this plugin.
+    """
+    config_val = conf.get(section=curr_section, key=curr_key)
+    logging.info(f"{ curr_section }.{ curr_key } is set to { config_val }")
+    assert config_val == expected_value
